924fa3116389f37ef43b4aac1eec190dd489d55a
altcios <alena596.tsios@yandex.ru>
2018-09-26 17:12:37 -0700

    nya

---
+++ FstApp/Home.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for Home.xaml
+    /// </summary>
+    public partial class Home : Page
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            ExpPR exp = new ExpPR(peopleListBox.SelectedItem);
+            NavigationService.Navigate(exp);
+
+        }
+    }
+
+}
